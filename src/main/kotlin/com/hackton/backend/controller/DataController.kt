9bdcfd937871c374938d0bf71a8b4917bac4b6f9
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-10-12 03:06:59 +0900

    feat :: controller

---
+++ src/main/kotlin/com/hackton/backend/controller/DataController.kt
@@ -0,0 +1,74 @@
+package com.hackton.backend.controller
+
+import com.hackton.backend.dto.response.KillResponse
+import com.hackton.backend.dto.response.QueryChampionInfoResponse
+import com.hackton.backend.dto.response.QueryDestroyTowerInfoResponse
+import com.hackton.backend.dto.response.QueryInhibitorBuildingInfoResponse
+import com.hackton.backend.dto.response.QueryMonsterInfoResponse
+import com.hackton.backend.dto.response.QueryWinnerResponse
+import com.hackton.backend.service.QueryChampionInfoService
+import com.hackton.backend.service.QueryDestroyTowerInfoService
+import com.hackton.backend.service.QueryInhibitorBuildingInfoService
+import com.hackton.backend.service.QueryMonsterInfoService
+import com.hackton.backend.service.QueryWinnerService
+import org.springframework.core.io.ClassPathResource
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.sql.Timestamp
+
+@RequestMapping("/data")
+@RestController
+class DataController(
+    private val queryChampionInfoService: QueryChampionInfoService,
+    private val queryMonsterInfoService: QueryMonsterInfoService,
+    private val queryInhibitorBuildingInfoService: QueryInhibitorBuildingInfoService,
+    private val queryWinnerService: QueryWinnerService,
+    private val queryDestroyTowerInfoService: QueryDestroyTowerInfoService,
+) {
+
+    @GetMapping("/kill")
+    fun getKillInfo(@RequestParam("time") timestamp: Timestamp): KillResponse? {
+//        return dataParsingService
+        return null
+    }
+
+    @GetMapping("/champion")
+    fun getChampionInfo(): QueryChampionInfoResponse {
+        return queryChampionInfoService.getChampionInfo()
+    }
+
+    @GetMapping("/monster")
+    fun getMonsterInfo(
+        @RequestParam("timeStamp")
+        timestamp: Int,
+    ): QueryMonsterInfoResponse? {
+        return queryMonsterInfoService.getMonsterInfo(timestamp)
+    }
+
+    @GetMapping("/inhibitor")
+    fun getInhibitorBuildingInfo(
+        @RequestParam("timeStamp")
+        timestamp: Int,
+    ): QueryInhibitorBuildingInfoResponse {
+        return queryInhibitorBuildingInfoService.getInhibitorBuildingInfo(timestamp)
+    }
+
+    @GetMapping("/winner")
+    fun getWinnerInfo(): QueryWinnerResponse {
+        return queryWinnerService.getWinnerInfo()
+    }
+
+    @GetMapping("/tower")
+    fun getTowerInfo(
+        @RequestParam("timeStamp")
+        timeStamp: Int,
+    ): QueryDestroyTowerInfoResponse {
+        return queryDestroyTowerInfoService.getDestroyTowerInfo(timeStamp)
+    }
+
+// TODO: 킬 관련, 시간 관련(이전, 이후)
+
+// TODO: 팀별 챔피언 이름, 팀 id
+}
